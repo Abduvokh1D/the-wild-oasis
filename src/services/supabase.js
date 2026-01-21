fb9c0a59a0e1bfda0953a9a65247867fe5ab7e47
Abduvokh1D <abduvokhideurope@gmail.com>
2026-01-21 20:39:11 +0500

    Connecting the supabase backend to the project

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = 'https://wiamonwwuekikoaiantx.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndpYW1vbnd3dWVraWtvYWlhbnR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgzOTUwMDMsImV4cCI6MjA4Mzk3MTAwM30.hwJVLOD5eowGKW-d2ugiTl0cKzQF56BZ6tzA3VQylCo";
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
