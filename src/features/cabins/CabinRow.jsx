5943cbf45aa26ca7a60e2dd7afcc2922754f2db8
Abduvokh1D <abduvokhideurope@gmail.com>
2026-02-07 17:16:14 +0500

    Displaying the toasts

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import {formatCurrency} from "../../utils/helpers"
 import { deleteCabin } from "../../services/apiCabins";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 const TableRow = styled.div`
   display: grid;
   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
@@ -46,12 +47,12 @@ function CabinRow({cabin}) {
   const {isLoading: isDeleting, mutate} = useMutation({
     mutationFn: deleteCabin,
     onSuccess: ()=>{
-      alert("Cabin succesfully deleted")
+      toast.success("Cabin succesfully deleted")
       queryClient.invalidateQueries({
         queryKey: ['cabins']
       })
     },
-       onError: (err) => alert(err.message)
+       onError: (err) => toast.error(err.message)
     
   })
   return (
