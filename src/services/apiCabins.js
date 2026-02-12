d8ddc455234ad4550c07fd4e57e6ac73c7201e46
Abduvokh1D <abduvokhideurope@gmail.com>
2026-02-12 08:50:10 +0500

    fixing the bug on uploading the image

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -18,15 +18,17 @@ export async function createCabin(newCabin) {
   // 1. Create cabin
   const { data, error } = await supabase
     .from("cabins")
-    .insert([{ ...newCabin, image: imagePath }]);
+    .insert([{ ...newCabin, image: imagePath }])
+    .select()
+    .single();
   if (error) {
     console.error(error);
-    throw new Error("Cabin could not be deleted");
+    throw new Error("Cabin could not be created");
   }
   // 2. Upload image
   const { error: storageError } = await supabase.storage
     .from("cabin-images")
-    .upload(imageName, newCabin);
+    .upload(imageName, newCabin.image);
   // 3. Delete the cabin IF there was an error uploading image
   if (storageError) {
     await supabase.from("cabins").delete().eq("id", data.id);
