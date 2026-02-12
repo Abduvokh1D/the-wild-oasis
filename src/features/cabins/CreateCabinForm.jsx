80f514428fa34ac338e0e10026650c8f8e9ac332
Abduvokh1D <abduvokhideurope@gmail.com>
2026-02-12 08:42:13 +0500

    Uploading the images to Supabase

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -1,13 +1,13 @@
-import Input from '../../ui/Input';
-import Form from '../../ui/Form';
-import Button from '../../ui/Button';
-import FileInput from '../../ui/FileInput';
-import Textarea from '../../ui/Textarea';
-import { useForm } from 'react-hook-form';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { createCabin } from '../../services/apiCabins';
-import toast from 'react-hot-toast';
-import FormRow from '../../ui/FormRow';
+import Input from "../../ui/Input";
+import Form from "../../ui/Form";
+import Button from "../../ui/Button";
+import FileInput from "../../ui/FileInput";
+import Textarea from "../../ui/Textarea";
+import { useForm } from "react-hook-form";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
+import FormRow from "../../ui/FormRow";
 
 function CreateCabinForm() {
   const { register, handleSubmit, reset, getValues, formState } = useForm();
@@ -17,15 +17,15 @@ function CreateCabinForm() {
   const { mutate, isLoading: isCreating } = useMutation({
     mutationFn: createCabin,
     onSuccess: () => {
-      toast.success('New cabin successfully created');
-      queryClient.invalidateQueries({ queryKey: ['cabins'] });
+      toast.success("New cabin successfully created");
+      queryClient.invalidateQueries({ queryKey: ["cabins"] });
       reset();
     },
     onError: (err) => toast.error(err.message),
   });
 
   function onSubmit(data) {
-    mutate(data);
+    mutate({ ...data, image: data.image[0] });
   }
   function onError(errors) {
     console.log(errors);
@@ -37,8 +37,8 @@ function CreateCabinForm() {
           type="text"
           id="name"
           disabled={isCreating}
-          {...register('name', {
-            required: 'This field is required',
+          {...register("name", {
+            required: "This field is required",
           })}
         />
       </FormRow>
@@ -47,11 +47,11 @@ function CreateCabinForm() {
           type="number"
           id="maxCapacity"
           disabled={isCreating}
-          {...register('maxCapacity', {
-            required: 'This field is required',
+          {...register("maxCapacity", {
+            required: "This field is required",
             min: {
               value: 1,
-              message: 'Capacity should be at least 1',
+              message: "Capacity should be at least 1",
             },
           })}
         />
@@ -62,11 +62,11 @@ function CreateCabinForm() {
           type="number"
           disabled={isCreating}
           id="regularPrice"
-          {...register('regularPrice', {
-            required: 'This field is required',
+          {...register("regularPrice", {
+            required: "This field is required",
             min: {
               value: 1,
-              message: 'Capacity should be at least 1',
+              message: "Capacity should be at least 1",
             },
           })}
         />
@@ -78,11 +78,11 @@ function CreateCabinForm() {
           id="discount"
           disabled={isCreating}
           defaultValue={0}
-          {...register('discount', {
-            required: 'This field is required',
+          {...register("discount", {
+            required: "This field is required",
             validate: (value) =>
               value <= getValues().regularPrice ||
-              'Discount should be less than regular price',
+              "Discount should be less than regular price",
           })}
         />
       </FormRow>
@@ -96,14 +96,20 @@ function CreateCabinForm() {
           id="description"
           disabled={isCreating}
           defaultValue=""
-          {...register('description', {
-            required: 'This field is required',
+          {...register("description", {
+            required: "This field is required",
           })}
         />
       </FormRow>
 
       <FormRow label="Cabin photo">
-        <FileInput id="image" accept="image/*" />
+        <FileInput
+          id="image"
+          accept="image/*"
+          {...register("image", {
+            required: "This field is required",
+          })}
+        />
       </FormRow>
 
       <FormRow>
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,40 +1,47 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
-export async function getCabins(){
-
-  
-const {  data, error } = await supabase
-  .from('cabins')
-  .select('*')
-  if(error){
-    console.error(error)
-    throw new Error("Cabins could not be loaded")
-  }
-return data
-}
-export async function createCabin(newCabin){
-  
-const { data, error } = await supabase
-  .from('cabins')
-  .insert([
-    newCabin
-  ]);
-  if(error){
-    console.error(error)
-    throw new Error("Cabin could not be deleted")
+export async function getCabins() {
+  const { data, error } = await supabase.from("cabins").select("*");
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be loaded");
   }
   return data;
-
 }
-export async function deleteCabin(id){
-  
-const {data, error  } = await supabase
-  .from('cabins')
-  .delete()
-  .eq('id', id)
- if(error){
-    console.error(error)
-    throw new Error("Cabin could not be deleted")
+export async function createCabin(newCabin) {
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    "/",
+    ""
+  );
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+  // https://wiamonwwuekikoaiantx.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg
+  // 1. Create cabin
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([{ ...newCabin, image: imagePath }]);
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be deleted");
+  }
+  // 2. Upload image
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin);
+  // 3. Delete the cabin IF there was an error uploading image
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    console.error(storageError);
+    throw new Error(
+      "Cabin image could not be uploaded and the cabin was not created"
+    );
+  }
+  return data;
+}
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be deleted");
   }
   return data;
 }
